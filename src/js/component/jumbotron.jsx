66609ddf1ea0af3d71e1450609d83ae553651081
Jimena <jparatge@gmail.com>
2024-10-11 02:51:33 +0000

    tercera version

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Jumbotron = () => {
     return (
-        <div className="mb-4 bg-body-tertiary rounded-3" >
+        <div className="mb-5 bg-light rounded-3 w-100" >
             <div className="container-fluid py-5" >
-                <h1 className="display-5 fw-bold" > A Warm Welcome! </h1>
+                <h1 className="display-5 fw-bold mt-4" > A Warm Welcome! </h1>
                 < p className="col-md-12 fs-4" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Non fugit libero vitae magni sequi nemo mollitia laboriosam corporis saepe natus, repellat ipsam. Placeat, iusto quas delectus consequatur reiciendis modi illo?
                 </p>
                 < button className="btn btn-primary btn-lg" type="button" > Call to action! </button>
--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -2,37 +2,35 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="container">
-      <nav className="navbar navbar-expand-lg bg-dark">
-        <div className="d-flex justify-content-between align-items-center w-100 m-1">
-          {/* <span className="spaced"> */}
-          <a className="navbar-brand text-light px-2" href="#">Start Bootstrap</a>
-          <button className="navbar-toggler text-white " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon text-white"></span>
-          </button>
-          {/* </span> */}
-
+      <div className="py-1 bg-dark position-fixed w-100">
+        <nav className="navbar navbar-expand-lg">
+          <div className="d-flex justify-content-between align-items-center w-100">
+           <a className="navbar-brand text-light px-2" href="#">Start Bootstrap</a>
+            <button className="navbar-toggler text-white " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon text-white"></span>
+            </button>
           <span className="spaced">
-            <div className="collapse navbar-collapse text-light" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link text-light" href="#">About</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link text-light" href="#">Services</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link text-light" href="#">Contact</a>
-                </li>
-              </ul>
-            </div>
-          </span>
-        </div>
-      </nav>
-    </div>
+              <div className="collapse navbar-collapse text-light" id="navbarNav">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link text-light" href="#">About</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link text-light" href="#">Services</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link text-light" href="#">Contact</a>
+                  </li>
+                </ul>
+              </div>
+            </span>
+          </div>
+        </nav>
+      </div>
+    // </div>
   );
 };
 
